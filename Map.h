c7def4ceb599d24df031cf27fce7acf91a2ab571
Mirna <MirnaMuhammad98@gmail.com>
2018-12-08 21:58:47 +0200

    Read + Built the graph
    1) Read from a file // Should be input later
    2) Create the Object that contains the Graph

--- Map.h
+++ Map.h
@@ -1,12 +1,26 @@
 #pragma once
 #include<iostream>
+#include<vector>
+#include<istream>
+#include<ostream>
+#include<sstream>
+#include<fstream>
+#include<string>
 using namespace std;
+
+typedef long double ld;
+const int N = 2e5+5; //maximum # of nodes, not actually used anywhere
 
 class Map
 {
 	//graph
 public:
-	Map(string fileName);
+	int n; //number of nodes 
+	int m; //number of edges
+	vector<pair<ld, ld>>node; //node[id] {X, Y}
+	vector<vector<pair<int, pair<ld, ld>>>>g; // g[id1] {id2, {Length, Time}}
+
+	Map(string fileName); //read + create
 	friend class Query;
 	~Map();
 };
--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,22 @@
+#include<iostream>
 #include"Map.h"
 #include"Query.h"
-
-int main() {
-
-	return 0;
+using namespace std;
+int main(){
+	 Map M("map1");
+	 // A temporary code to access and view the data
+	 /* 
+	 for(int i=0 ; i < M.n ; i++){
+		 cout << "Node " << i << ": " << M.node[i].first << ' ' << M.node[i].second << '\n';
+	 }
+	 cout << "\nConnections between: \n"; 
+	 for(int i=0 ; i < M.n ; i++){
+		 cout << i << ": ";
+		 for(int j=0 ; j < M.g[i].size() ; j++){
+			 cout << M.g[i][j].first << ' ';
+		 }
+		 cout << '\n';
+	 }
+	 */
+ return 0;
 }
